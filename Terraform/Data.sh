25a35d3d1326058e7a612400c457d85ca4f62a32
Kervin Prinville <prinvillek@mail.com>
2023-11-11 22:11:23 -0500

    Changed Data.sh, Changed main.tf

--- Terraform/Data.sh
+++ Terraform/Data.sh
@@ -1,8 +1,68 @@
 #!/bin/bash
-sudo curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo apt-get install cloud-utils
-sudo docker pull kerv17/flask-app:latest
-EC2_INSTANCE_ID=$(ec2metadata --instance-id)
-EC2_TYPE=$(ec2metadata  --instance-type)
-sudo docker run -e instanceId="$EC2_INSTANCE_ID / $EC2_TYPE" -d -p 80:5000 kerv17/flask-app:latest
+echo "Apt update"
+sudo apt update
+echo "Installing Java"
+sudo apt-get -y install default-jdk default-jre
+
+echo "Installing Hadoop"
+wget https://downloads.apache.org/hadoop/common/hadoop-3.3.1/hadoop-3.3.1.tar.gz
+tar -xvzf hadoop-3.3.1.tar.gz
+sudo mv hadoop-3.3.1 /usr/local/hadoop
+rm -r hadoop-3.3.1.tar.gz
+
+git clone https://github.com/kerv17/CloudTP2
+cd CloudTP2
+git checkout test
+
+echo "Setting Hadoop environment variables"
+export HADOOP_HOME=/usr/local/hadoop
+export PATH=$PATH:/usr/local/hadoop/sbin:/usr/local/hadoop/bin
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+
+# PG4300.TXT experiment
+echo "Wordcount on pg4300.txt with hadoop"
+{ time hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.1.jar wordcount pg4300.txt output_pg4300 ; } 2> time_hadoop_pg4300.txt
+
+echo "Wordcount on pg4300.txt with linux cat"
+{ time cat pg4300.txt | tr ' ' '\n' | sort | uniq -c ; } 2> time_linux_pg4300.txt
+
+echo "Installing tinyurl datasets in input_datasets"
+hdfs dfs -mkdir input_datasets
+cd input_datasets
+wget https://tinyurl.com/4vxdw3pa
+wget https://tinyurl.com/kh9excea
+wget https://tinyurl.com/dybs9bnk
+wget https://tinyurl.com/datumz6m
+wget https://tinyurl.com/j4j4xdw6
+wget https://tinyurl.com/ym8s5fm4
+wget https://tinyurl.com/2h6a75nk
+wget https://tinyurl.com/vwvram8
+wget https://tinyurl.com/weh83uyn
+cd ..
+
+echo "Wordcount on tinyurl datasets with hadoop"
+{ time hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.1.jar wordcount input_datasets output_hadoop_datasets ; } 2> time_hadoop_datasets_1.txt
+sudo rm -r output_hadoop_datasets
+{ time hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.1.jar wordcount input_datasets output_hadoop_datasets ; } 2> time_hadoop_datasets_2.txt
+sudo rm -r output_hadoop_datasets
+{ time hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.1.jar wordcount input_datasets output_hadoop_datasets ; } 2> time_hadoop_datasets_3.txt
+
+
+echo "Installing spark"
+wget https://dlcdn.apache.org/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz
+tar -xvzf spark-3.3.1-bin-hadoop3.tgz
+sudo mv spark-3.3.1-bin-hadoop3 /usr/local/spark
+sudo rm -r spark-3.3.1-bin-hadoop3.tgz
+export SPARK_HOME=/usr/local/spark
+export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
+export PYSPARK_PYTHON=/usr/bin/python3
+
+echo "Wordcount on tinyurl datasets with pyspark"
+{ time spark-submit Wordcount/wordcount_spark.py input_datasets output_spark_datasets ; } 2> time_spark_datasets_1.txt
+sudo rm -r output_spark_datasets
+{ time spark-submit Wordcount/wordcount_spark.py input_datasets output_spark_datasets ; } 2> time_spark_datasets_2.txt
+sudo rm -r output_spark_datasets
+{ time spark-submit Wordcount/wordcount_spark.py input_datasets output_spark_datasets ; } 2> time_spark_datasets_3.txt
+
+#echo "Run friends.py"
+#spark-submit friends.py
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,8 +5,7 @@ variable "aws_region" {
 
 variable "ami_id" {
   description = "The AMI ID for the instances"
-  default     = "ami-0149b2da6ceec4bb0" // ami_id for us-east-1 linux 
-  
+  default     = "ami-0149b2da6ceec4bb0"// Replace with Ubuntu AMI ID for us-east-1
 }
 
 variable "Data_path" {
@@ -54,12 +53,15 @@ variable "token" {
   description = "AWS session token (optional)"
 }
 
-
 provider "aws" {
   region = var.aws_region
   access_key = var.access_key
   secret_key = var.secret_key
   token = var.token
+}
+
+data "aws_vpc" "default" {
+  default = true
 }
 
 resource "aws_security_group" "security_gp" {
@@ -81,9 +83,9 @@ resource "aws_security_group" "security_gp" {
   }
 }
 
-
-data "aws_vpc" "default" {
-  default = true
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-key-name"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxWUIcTnH5DnbvIFVi97bP8My3es+ir283o+8W6soQgfPkAEJT6yl6yGDvgmZIgu2jTB8C6zEgXkCtRTCytU+Vbe1l6++duaDQrOFFA06ID4GE39wob58TDT0DIKz7QAHvBMCabYnLlNi8XOHaZEO7WxlO9pgC9CO51OUxQoNu1yQgsW82RmsKxAszT5+PBd4T5G0Z6U98Yp9uqBOl+ntE9DmFfqlahCheW5jkm6xmXn/y3+IPW4pl32Q/gqkAlbsVNVMbkCwqST/j3amw7OEiSjO+E8nXWKqgnMTxi1K7stWokcZlEz0gEkHgw23SnxlaTTcmYstpdg0uNiYBraop rsa-key-20231111"
 }
 
 resource "aws_instance" "instances_m4" {
@@ -93,30 +95,11 @@ resource "aws_instance" "instances_m4" {
   availability_zone      = "us-east-1c"
   user_data              = file(var.Data_path)
   count                  = var.m4_instance_count
+  key_name               = aws_key_pair.my_key.key_name
 
   tags = merge(var.instance_tags, {
     "Name" = "M4"
   })
-}
-
-resource "aws_instance" "instances_t2" {
-  ami                    = var.ami_id
-  instance_type          = "t2.large"
-  vpc_security_group_ids = [aws_security_group.security_gp.id]
-  availability_zone      = "us-east-1d"
-  user_data              = file(var.Data_path)
-  count                  = var.t2_instance_count
-
-  tags = merge(var.instance_tags, {
-    "Name" = "T2"
-  })
-}
-
-data "aws_subnets" "all" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
 }
 
 output "instance_dns" {
